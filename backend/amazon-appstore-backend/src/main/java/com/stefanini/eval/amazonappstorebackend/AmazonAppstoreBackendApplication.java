3ed6db6f1e5531a80cdb4d3fad30cf20d5c2eb72
unknown <devecm4305@gmail.com>
2023-07-29 21:33:38 -0600

    Primer commit backend.
    Creación de proyecto java basado en spring boot para backend de
    aplicación de tienda en línea con una base de datos en memoria.

---
+++ backend/amazon-appstore-backend/src/main/java/com/stefanini/eval/amazonappstorebackend/AmazonAppstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.eval.amazonappstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonAppstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonAppstoreBackendApplication.class, args);
+	}
+
+}
